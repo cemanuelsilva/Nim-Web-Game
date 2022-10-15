0ca1d662fd471e734117bb088b73b65e74889b13
klara <klara.sofia@hotmail.com>
2022-10-15 18:41:33 +0100

    2ª aula: iniciar e mostrar tabuleiro

--- public_html/config.js
+++ public_html/config.js
@@ -1,38 +1,59 @@
-class Nim {
-    constructor(id) {
-        this.content = new Array(col);
-        this.column = new Array(col);
-        // ++++++
+
+function  setGame(){
+  var selId = document.getElementById('size');
+
+  let col = parseInt(selId.options[selId.selectedIndex].value);
+
+  const board = document.getElementById('board');
+
+  for(let i=0, c=col; i<col;i++,c--) {
+    const column = document.createElement('div');
+    column.className = 'column';
+    board.appendChild(column);
+    for(let j=0; j<c; j++) {
+      const piece = document.createElement('div');
+      piece.className = 'piece';
+      column.appendChild(piece);
     }
+  }
 }
 
-constructor(id){
-
-    const parent = document.getElemenyById(id)
-    const column = document.createElement('div')
-
-    board.Nim = 'board';
-    parent.appendChild(column); 
+function appearBoard() {
+  var b = document.getElementById('board');
+  if (b.style.display === "none") {
+      board.style.display = "flex";
+      setGame();
+  }
 }
 
-constructor(id) {
-    for(let i=0; i < col; i++){
-        let piece = document.createElement("div");
-        piece.Nim = "cell";
-        column.appendChild(piece);
-
-        piece.onclick = () => this.play(i);
-
-        this.column[i] = piece;
-    }
+function disappearBoard() {
+  var b = document.getElementById('board');
+  while(b.firstChild) {
+    b.removeChild(b.firstChild);
+  }
+  board.style.display="none";
 }
 
-// play(pos){
-//     this.content[pos] = this.current
+function toggleText() {
+  var text = document.getElementById("rules");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+  text.style.display = "none";
+  }
+}
 
-// }
+document.addEventListener('DOMContentLoaded', function () {
+ var input = document.getElementById('size');
+ if (localStorage['size']) { // if size is set
+     input.value = localStorage['size']; // set the value
+ }
+ input.onchange = function () {
+      localStorage['size'] = this.value; // change localStorage on change
+  }
+})
+
 
 window.onload = function() {
-    const counter = new Nim("Base");
-}
 
+}
